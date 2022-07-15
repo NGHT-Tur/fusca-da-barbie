d80b57f767a120954b998745bfc5477ebfb12d3a
NGHT-Tur <104224519+NGHT-Tur@users.noreply.github.com>
2022-07-15 15:49:07 -0300

    15-07-2022

--- js/Form.js
+++ js/Form.js
@@ -1,16 +1,16 @@
 class Form {
   constructor() {
-    this.input = createInput("").attribute("placeholder", "Digite seu nome");
+    this.input = createInput("").attribute("placeholder", "Digite Seu Nome");
     this.playButton = createButton("Jogar");
-    this.titleImg = createImg("./assets/TITULO.png", "nome do jogo");
+    this.titleImg = createImg("./assets/TITULO.png", "game title");
     this.greeting = createElement("h2");
   }
 
   setElementsPosition() {
-    this.titleImg.position(120,50);
+    this.titleImg.position(120, 50);
     this.input.position(width / 2 - 110, height / 2 - 80);
     this.playButton.position(width / 2 - 90, height / 2 - 20);
-    this.greeting.position(width / 2 -500, height / 2 - 100);
+    this.greeting.position(width / 2 - 300, height / 2 - 100);
   }
 
   setElementsStyle() {
@@ -20,19 +20,26 @@ class Form {
     this.greeting.class("greeting");
   }
 
-  handleMousePressed(){
-    this.playButton.mousePressed(()=>{
-    this.playButton.hide();
-    this.input.hide();
-    var zapzap = `Hi! ${this.input.value()}</br> espere o amiguinho se não você vai ter que se retirar.`;
-    this.greeting.html(zapzap);
-    });
-  }
-
   hide() {
     this.greeting.hide();
     this.playButton.hide();
     this.input.hide();
+  }
+
+  handleMousePressed() {
+    this.playButton.mousePressed(() => {
+      this.input.hide();
+      this.playButton.hide();
+      var message = `
+      Olá ${this.input.value()}
+      </br>espere o outro jogador entrar...`;
+      this.greeting.html(message);
+      playerCount += 1;
+      player.name = this.input.value();
+      player.index = playerCount;
+      player.addPlayer();
+      player.updateCount(playerCount);
+    });
   }
 
   display() {
